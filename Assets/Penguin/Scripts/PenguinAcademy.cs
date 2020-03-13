655abfcadb7cee494c69aa86a27bd30654d883d3
Kamil Chruścinski <kamil.chruscinski@gmail.com>
2020-03-14 00:59:27 +0100

    + Added PenguinAcademy + Added PenguinAgent + Added PenguinArea + Added Fish

---
+++ Assets/Penguin/Scripts/PenguinAcademy.cs
@@ -0,0 +1,39 @@
+﻿using MLAgents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinAcademy : MonoBehaviour
+{
+    private void Start()
+    {
+        FishSpeed = 0f;
+        FeedRadius = 0f;
+
+        // Set up code to be called every time the fish_speed parameter changes 
+        // during curriculum learning
+        Academy.Instance.FloatProperties.RegisterCallback("fish_speed", f =>
+        {
+            FishSpeed = f;
+        });
+
+        // Set up code to be called every time the feed_radius parameter changes 
+        // during curriculum learning
+        Academy.Instance.FloatProperties.RegisterCallback("feed_radius", f =>
+        {
+            FeedRadius = f;
+        });
+    }
+
+
+    /// <summary>
+    /// Gets/sets the current fish speed
+    /// </summary>
+    public float FishSpeed { get; private set; }
+
+    /// <summary>
+    /// Gets/sets the current acceptable feed radius
+    /// </summary>
+    public float FeedRadius { get; private set; }
+
+}
